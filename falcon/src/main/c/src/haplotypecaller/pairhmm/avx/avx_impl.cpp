5261e65794755929266ed677c52e6798be2fa2a1
AriesLL <memoryzpp@gmail.com>
2018-07-25 15:40:09 -0700

    Revert "add falcon integration, fix build.gradle"
    This reverts commit 73719a3ed6ca14d82a8f9c1fc63dfb44f449b79f.
    
    revert the changes to release

--- falcon/src/main/c/src/haplotypecaller/pairhmm/avx/avx_impl.cpp
+++
@@ -1,7 +0,0 @@
-#include "pairhmm/avx/avx_impl.h"
-#include "pairhmm/avx/avx-pairhmm.h"
-#include "pairhmm/common/pairhmm_common.h"
-
-float (*compute_fp_avxs)(testcase*) = &compute_full_prob_avxs;
-double (*compute_fp_avxd)(testcase*) = &compute_full_prob_avxd;
-
