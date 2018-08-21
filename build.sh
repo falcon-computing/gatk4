dc065c4b3216b825efd78b6a7cabfa4e93384859
AriesLL <memoryzpp@gmail.com>
2018-08-20 19:20:35 -0700

    add build.sh

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rm -f build.log
+
+# build falcon_genomics
+cd ./falcon
+
+echo "building falcon_genomics..."
+if [ $# -gt 0 ]; then
+      cloud=$1
+        ./gradlew clean install -Prelease -Pcloud=$cloud &>> ../build.log
+    else
+          ./gradlew clean install -Prelease &>> ../build.log
+      fi  
+      version=$(cat ../build.log | grep "Version:" | awk '{print $2}')
+
+      echo "falcon genomics version: $version"
+      cd -
+
+      echo "building gatk..."
+      #mvn clean package -Ddisable.queue -Dfalcon-genomics.version="$version" &>> build.log
+      ./gradlew bundle &>> build.log
+      echo "installing gatk to ~/.falcon-genome/gatk/$version"
+      mkdir -p ~/.falcon-genome/gatk/$version
+      cp build/libs/gatk.jar  ~/.falcon-genome/gatk/$version/
+
+
