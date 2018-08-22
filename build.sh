716b201af1a8493ca218ff99c4b0b71166d24e56
Di Wu <allwu@falcon-computing.com>
2018-08-22 15:50:22 -0700

    update build.sh to be consistent with current genomics build process

--- build.sh
+++ build.sh
@@ -7,19 +7,19 @@ cd ./falcon
 
 echo "building falcon_genomics..."
 if [ $# -gt 0 ]; then
-	  cloud=$1
-	    ./gradlew clean install -Prelease -Pcloud=$cloud &>> ../build.log
-	else
-		  ./gradlew clean install -Prelease &>> ../build.log
-	  fi
-	  version=$(cat ../build.log | grep "Version:" | awk '{print $2}')
+  cloud=$1
+  ./gradlew clean install -Prelease -Pcloud=$cloud &>> ../build.log
+else
+  ./gradlew clean install -Prelease &>> ../build.log
+fi
+version=$(cat ../build.log | grep "Version:" | awk '{print $2}')
 
-	  echo "falcon genomics version: $version"
-	  cd -
+echo "falcon genomics version: $version"
+cd -
 
-	  echo "building gatk..."
-	  #mvn clean package -Ddisable.queue -Dfalcon-genomics.version="$version" &>> build.log
-      ./gradlew bundle -Dfalcon.version="$version" &>> build.log
-	  echo "installing gatk to ~/.falcon-genome/gatk/$version"
-	  mkdir -p ~/.falcon-genome/gatk/$version
-	  cp build/libs/gatk.jar  ~/.falcon-genome/gatk/$version/
+echo "building gatk..."
+./gradlew bundle -Dfalcon.version="$version" &>> build.log
+
+echo "installing gatk"
+mkdir -p ./export/
+cp build/libs/gatk.jar ./export/
