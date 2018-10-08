fb257a8ed5fe19131624196440419b48170032ef
Di Wu <allwu@falcon-computing.com>
2018-10-08 15:43:14 -0700

    add build option for profiling and platform selection

--- build.sh
+++ build.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
+flags="-Prelease"
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+  -p|--platform)
+    flags="$flags -Pcloud=$2"
+    shift
+    ;;
+  -f|--profiling)
+    flags="$flags -Pprofiler"
+    ;;
+  *)
+    # unknown option
+    echo "Failed to recongize argument '$1'"
+    exit 1
+    ;;
+  esac
+  shift # past argument or value
+done
+
 rm -f build.log
 
 # build falcon_genomics
 cd ./falcon
 
 echo "building falcon_genomics..."
-if [ $# -gt 0 ]; then
-  cloud=$1
-  ./gradlew clean install -Prelease -Pcloud=$cloud &>> ../build.log
-else
-  ./gradlew clean install -Prelease &>> ../build.log
-fi
+./gradlew clean install $flags &>> ../build.log
 version=$(cat ../build.log | grep "Version:" | awk '{print $2}')
 
 echo "falcon genomics version: $version"
