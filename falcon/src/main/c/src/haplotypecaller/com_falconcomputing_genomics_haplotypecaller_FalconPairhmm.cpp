463701c3a3d057e11416c57ac0ceffcb40500f6c
Di Wu <allwu@falcon-computing.com>
2018-10-09 15:45:15 -0700

    separate pairhmm client from repo (#6)

--- falcon/src/main/c/src/haplotypecaller/com_falconcomputing_genomics_haplotypecaller_FalconPairhmm.cpp
+++ falcon/src/main/c/src/haplotypecaller/com_falconcomputing_genomics_haplotypecaller_FalconPairhmm.cpp
@@ -1,81 +1,101 @@
-#include <vector>
 #include <algorithm>
-#include <math.h>
 #include <assert.h>
+#include <map>
+#include <math.h>
+#include <mutex>
 #include <stdio.h>
 #include <stdlib.h>
+#include <thread>
+#include <unordered_map>
+#include <vector>
+#include <xmmintrin.h>
 
+#include "Common.h"
 #include "com_falconcomputing_genomics_haplotypecaller_FalconPairhmm.h"
-#include "gkl-pairhmm/Context.h"
 #include "gkl-pairhmm/avx_impl.h"
 #include "ksight/tools.h"
-#include "pairhmm/client/PairhmmClient.h"
-#include "pairhmm/client/PairhmmWorker.h"
-#include "pairhmm/common/PairhmmJavaData.h"
-#include <xmmintrin.h>
+#include "pairhmm/PairHMMClient.h"
+#include "pairhmm/PairHMMWorker.h"
+#include "pairhmm/PairhmmJavaData.h"
+
+std::mutex pmm_client_map_mutex;
+std::unordered_map<std::thread::id, PairHMMClient*> pmm_client_map;
+
+PairHMMClient* get_pairhmm_client() {
+    std::thread::id this_id = std::this_thread::get_id();
+    PairHMMClient* client;
+    pmm_client_map_mutex.lock();
+    std::unordered_map<std::thread::id, PairHMMClient*>::iterator iter = pmm_client_map.find(this_id);
+    if(iter == pmm_client_map.end()){
+        client = new PairHMMClient();
+        pmm_client_map[this_id] = client;
+    }
+    else{
+        client = iter->second;
+    }
+    pmm_client_map_mutex.unlock();
+    return client;
+}
+
+void release_pairhmm_client() {
+    std::thread::id this_id = std::this_thread::get_id();
+    pmm_client_map_mutex.lock();
+    std::unordered_map<std::thread::id, PairHMMClient*>::iterator iter = pmm_client_map.find(this_id);
+    if (iter != pmm_client_map.end()) {
+        PairHMMClient* client = iter->second;
+        delete client;
+        pmm_client_map.erase(this_id);   
+    }
+    pmm_client_map_mutex.unlock();
+}
 
 JNIEXPORT void JNICALL Java_com_falconcomputing_genomics_haplotypecaller_FalconPairhmm_initNative
 (JNIEnv* env, jclass cls, jclass readDataHolder, jclass haplotypeDataHolder, jboolean use_double, jint max_threads, jboolean use_fpga){
 
-    PairhmmContext* context = get_pairhmm_context();
     PairHMMClient* client = get_pairhmm_client();
     JavaData javaData;
     javaData.init(env, readDataHolder, haplotypeDataHolder);
-    context->g_use_double = use_double;
 
     // enable FTZ
     _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
 
     // init convert char table
     ConvertChar::init();
-    for(int i = 0; i < 3; i++)
-        inputBank[i].first = "bankID";
-#ifdef DEPLOY_aws
-    inputBank[0].second = 3; inputBank[1].second = 1; inputBank[2].second = 0;
-#else
-    inputBank[0].second = 0; inputBank[1].second = 1; inputBank[2].second = 3;
-#endif
 }
 
 
 JNIEXPORT void JNICALL Java_com_falconcomputing_genomics_haplotypecaller_FalconPairhmm_computeLikelihoodsNative
-(JNIEnv* env, jobject obj, jobjectArray readDataArray, jobjectArray haplotypeDataArray, jdoubleArray likelihoodArray){
-    PLACE_TIMER;
-    //==================================================================
-    // get Java data
-    PairhmmContext* context = get_pairhmm_context();
-    int numRead = env->GetArrayLength(readDataArray);
-    int numHap = env->GetArrayLength(haplotypeDataArray);
-    bool useFPGA = !context->g_use_double;
-    JavaData javaData;
-    javaData.getData(env, readDataArray, haplotypeDataArray, context->reads, context->haps, numRead, numHap, useFPGA);
-    context->likelihoods = javaData.getOutputArray(env, likelihoodArray);
+(JNIEnv* env, jobject obj, jobjectArray readDataArray, jobjectArray haplotypeDataArray, jdoubleArray likelihoodArray) {
+  PLACE_TIMER;
 
-    //==================================================================
-    // call PairHMM worker
-    context->pmm_count++;
-    PairHMMClient* client = get_pairhmm_client();
-    PairHMMWorker worker(client, numRead, numHap, &context->reads[0], &context->haps[0]);
-    worker.run();
-    
-    //==================================================================
-    // copy output to Java data
-    worker.getOutput(context->likelihoods);
+  // get Java data
+  int num_read = env->GetArrayLength(readDataArray);
+  int num_hap  = env->GetArrayLength(haplotypeDataArray);
 
-    //==================================================================
-    // release Java data
-    javaData.releaseData(env);
+  std::vector<read_t> reads;
+  std::vector<hap_t>  haps;
+
+  JavaData javaData;
+  javaData.getData(env, readDataArray, haplotypeDataArray, reads, haps, num_read, num_hap);
+  double* likelihoods = javaData.getOutputArray(env, likelihoodArray);
+
+
+  // call PairHMM worker
+  PairHMMClient* client = get_pairhmm_client();
+  PairHMMWorker worker(client, num_read, num_hap, &reads[0], &haps[0]);
+  worker.run();
+
+  // copy output to Java data
+  worker.getOutput(likelihoods);
+
+  // release Java data
+  javaData.releaseData(env);
 }
 
 
-
-JNIEXPORT void JNICALL Java_com_falconcomputing_genomics_haplotypecaller_FalconPairhmm_doneNative
-(JNIEnv* env, jobject obj)
-{
+JNIEXPORT void JNICALL Java_com_falconcomputing_genomics_haplotypecaller_FalconPairhmm_doneNative(JNIEnv* env, jobject obj) {
 #ifndef NO_PROFILE
-    ksight::ksight.print_total(); 
+  ksight::ksight.print_total(); 
 #endif
-
-    release_pairhmm_context();
-    release_pairhmm_client();
+  release_pairhmm_client();
 }
